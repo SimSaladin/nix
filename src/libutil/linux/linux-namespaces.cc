1121fcfc1d9f054076135bad525f41c7e7fa5566
Samuli Thomasson <samuli.thomasson@pm.me>
2025-05-31 06:51:24 +0200

    disable ns test
    Signed-off-by: Samuli Thomasson <samuli.thomasson@pm.me>

--- src/libutil/linux/linux-namespaces.cc
+++ src/libutil/linux/linux-namespaces.cc
@@ -68,15 +68,15 @@ bool mountAndPidNamespacesSupported()
             Pid pid = startProcess([&]()
             {
                 /* Make sure we don't remount the parent's /proc. */
-                if (mount(0, "/", 0, MS_PRIVATE | MS_REC, 0) == -1)
-                    _exit(1);
+                /*if (mount(0, "/", 0, MS_PRIVATE | MS_REC, 0) == -1)
+                    _exit(1);*/
 
                 /* Test whether we can remount /proc. The kernel disallows
                    this if /proc is not fully visible, i.e. if there are
                    filesystems mounted on top of files inside /proc.  See
                    https://lore.kernel.org/lkml/87tvsrjai0.fsf@xmission.com/T/. */
-                if (mount("none", "/proc", "proc", 0, 0) == -1)
-                    _exit(2);
+                /*if (mount("none", "/proc", "proc", 0, 0) == -1)
+                    _exit(2);*/
 
                 _exit(0);
             }, {
